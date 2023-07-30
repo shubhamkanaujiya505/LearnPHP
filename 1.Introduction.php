53cc8846db506aca7e04bea10facee5a5aeb0e10
Shubham Kanaujiya <shubhamkanaujiya505@gmail.com>
2023-07-30 11:18:09 +0530

    add 1 to 6

---
+++ 1.Introduction.php
@@ -0,0 +1,32 @@
+<!-- 
+PHP  -> Hypertext preprocessor
+create dynamic website with database
+web based software application
+
+ -> why we learn php
+        1. ease of use and learn
+        2. cost efficiency
+        3. OOPs
+        4. cross plateform compatibility
+        5. work with all servers apache iis etc
+        6. security
+        7. huge community
+
+-> uses of php in web development
+        1. cms (content managment system)
+        2. E-commerce website
+        3. mail server
+        4. chatting system
+        5. file storage system
+        6. ERP(enterprise resiurce planning)
+        7. crm(customer relationship management)
+
+->  prior knowladge before learning php
+        1. html
+        2. css (basic)
+        3. javascript(optional)
+
+
+
+
+-->
---
+++ 4.Echo_Print.php
@@ -0,0 +1,20 @@
+<!-- php echo statement -->
+<?php
+echo "hello world<br>";
+echo 'hello world <br>';
+echo "hello" . "world<br>";
+echo "hello" , "world<br>";
+echo "<b> Hello world </b><br>";
+echo 12.34;
+
+// echo statement is faster than print statement
+
+print "hello world<br>";
+print 'hello world <br>';
+print "hello" . "world<br>";
+// print "hello" . "world<br>"; // , are not allowed in print
+print "<b> Hello world </b><br>";
+print 12.34
+
+
+?>
