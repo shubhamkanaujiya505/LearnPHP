251c7196005603228f5c24edc31d6c1e15346b18
shubham.kanaujiya <shubham.kanaujiya@gingerwebs.co.in>
2023-08-03 23:57:09 +0530

    php 22 to 24 file

---
+++ 24.Goto_Statement.php
@@ -0,0 +1,27 @@
+<?php
+// $a = 1;
+// for($a = 1; $a > 20; $a++){
+//     if ($a == 10){
+//         goto abc;
+//     }
+//     echo $a . "<br>";
+// }
+
+echo "I am in goto statement1";
+echo "I am in goto statement2";
+echo "I am in goto statement3";
+echo "I am in goto statement4";
+goto abc;
+echo "I am in goto statement5";
+echo "I am in goto statement6";
+echo "I am in goto statement7";
+echo "I am in goto statement8";
+echo "I am in goto statement9";
+abc:
+echo "this is the last statement of goto. "
+
+
+
+
+
+?>
